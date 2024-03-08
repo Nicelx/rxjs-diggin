0bc9bad3d9bbcb1ede7a81fcbf56a2aa06514e65
Nicelx <relush321@gmail.com>
2024-03-08 23:46:32 +0300

    odata filters, odata2ts

--- src/app/trippin.service.ts
+++ src/app/trippin.service.ts
@@ -1,6 +1,6 @@
 import { Inject, Injectable } from "@angular/core";
 import { Observable, map, of } from "rxjs";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { TRIPPIN_BASE_URL } from "./app.config";
 
 interface Result {
@@ -16,8 +16,19 @@ export class TrippinService {
 		@Inject(TRIPPIN_BASE_URL) private baseUrl: string
 	) {}
 
-	public getPeople(nameFilter?: string): Observable<{value: any[]}> {
-		return this.httpClient
-			.get<{value: any[]}>(`${this.baseUrl}/People`)
+	public getPeople(nameFilter?: string): Observable<{ value: any[] }> {
+		// return this.httpClient.get<{ value: any[] }>(`${this.baseUrl}/People`);
+    let params = new HttpParams();
+    if (nameFilter) {
+      params = params.set(
+        '$filter',
+        `contains(FirstName, '${nameFilter}') or contains(LastName, '${nameFilter}') or (MiddleName ne null and contains(MiddleName, '${nameFilter}'))`
+      );
+    }
+    params = params.set('$select', 'UserName,FirstName,MiddleName,LastName,Age');
+    params = params.set('$orderby', 'LastName,FirstName');
+
+    return this.httpClient
+      .get<{ value: any[] }>(`${this.baseUrl}/People`, { params })
 	}
 }
